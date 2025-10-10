c9918d08a590def3bf5c64763ecbfd1002c366ee
Daryl <daryllai28@gmail.com>
2025-10-10 13:29:02 +0800

    Updated webscraping py file

--- web_scrapping/web_scrapping_final.py
+++ web_scrapping/web_scrapping_final.py
@@ -1,13 +1,9 @@
-# %%
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from pathlib import Path  
 
-# %% [markdown]
-# #1. Qatar Airways
-
-# %%
+# Function for web scraping (Qatar Airways)
 def scrape_page_QA(page_number):
     url = f'https://www.airlinequality.com/airline-reviews/qatar-airways/page/{page_number}/'
     response = requests.get(url)
@@ -36,10 +32,10 @@ def scrape_page_QA(page_number):
 
     return reviews
 
-# %%
-# Main loop to scrape multiple pages and track progress
+# Main loop for qatar airways to scrape multiple pages and track progress
 qatar_airways_reviews = []
 total_pages = 264
+print("Scrapping for Qatar Airways")
 for page_number in range(1, total_pages + 1):
     page_reviews = scrape_page_QA(page_number)
     qatar_airways_reviews.extend(page_reviews)
@@ -48,20 +44,9 @@ for page_number in range(1, total_pages + 1):
     if page_number % 10 == 0:
         print(f"{page_number} pages extraction completed")
 
-
-# %%
 qatar_airways_reviews_df = pd.DataFrame(qatar_airways_reviews)
 
-# %%
-qatar_airways_reviews_df.sample(5)
-
-# %%
-qatar_airways_reviews_df.shape
-
-# %% [markdown]
-# #2. Singapore Airlines
-
-# %%
+# Function for web scraping (Singapore Airlines)
 def scrape_page_SQ(page_number):
     url = f'https://www.airlinequality.com/airline-reviews/singapore-airlines/page/{page_number}/'
     response = requests.get(url)
@@ -90,9 +75,10 @@ def scrape_page_SQ(page_number):
 
     return reviews
 
-# %%
+# Main loop for singapore airlines to scrape multiple pages and track progress
 singapore_airlines_reviews = []
 total_pages = 167
+print("Scrapping for Singapore Airlines")
 for page_number in range(1, total_pages + 1):
     page_reviews = scrape_page_SQ(page_number)
     singapore_airlines_reviews.extend(page_reviews)
@@ -102,19 +88,9 @@ for page_number in range(1, total_pages + 1):
         print(f"{page_number} pages extraction completed")
 
 
-# %%
 singapore_airlines_reviews_df = pd.DataFrame(singapore_airlines_reviews)
 
-# %%
-singapore_airlines_reviews_df.sample(5)
-
-# %%
-singapore_airlines_reviews_df.shape
-
-# %% [markdown]
-# #3. Cathay Pacific Airways
-
-# %%
+#Function for web scraping (Cathay pacific Airways)
 def scrape_page_CP(page_number):
     url = f'https://www.airlinequality.com/airline-reviews/cathay-pacific-airways/page/{page_number}/'
     response = requests.get(url)
@@ -143,9 +119,10 @@ def scrape_page_CP(page_number):
 
     return reviews
 
-# %%
+# Main loop for cathay pacific airways to scrape multiple pages and track progress
 cathay_pacific_airways_reviews = []
 total_pages = 151
+print("Scrapping for Cathay Pacific Airways")
 for page_number in range(1, total_pages + 1):
     page_reviews = scrape_page_CP(page_number)
     cathay_pacific_airways_reviews.extend(page_reviews)
@@ -154,20 +131,9 @@ for page_number in range(1, total_pages + 1):
     if page_number % 10 == 0:
         print(f"{page_number} pages extraction completed")
 
-
-# %%
 cathay_pacific_airways_reviews_df = pd.DataFrame(cathay_pacific_airways_reviews)
 
-# %%
-cathay_pacific_airways_reviews_df.sample(5)
-
-# %%
-cathay_pacific_airways_reviews_df.shape
-
-# %% [markdown]
-# #4. Emirates
-
-# %%
+#Function for web scraping (Emirates)
 def scrape_page_EM(page_number):
     url = f'https://www.airlinequality.com/airline-reviews/emirates/page/{page_number}/'
     response = requests.get(url)
@@ -196,9 +162,10 @@ def scrape_page_EM(page_number):
 
     return reviews
 
-# %%
+# Main loop for emirates to scrape multiple pages and track progress
 emirates_reviews = []
 total_pages = 247
+print("Scrapping for Emirates")
 for page_number in range(1, total_pages + 1):
     page_reviews = scrape_page_EM(page_number)
     emirates_reviews.extend(page_reviews)
@@ -207,20 +174,9 @@ for page_number in range(1, total_pages + 1):
     if page_number % 10 == 0:
         print(f"{page_number} pages extraction completed")
 
-
-# %%
 emirates_reviews_df = pd.DataFrame(emirates_reviews)
 
-# %%
-emirates_reviews_df.sample(5)
-
-# %%
-emirates_reviews_df.shape
-
-# %% [markdown]
-# #5.ANA All Nippon Airways
-
-# %%
+#Function for web scraping (ANA All Nippon Airways)
 def scrape_page_ANA(page_number):
     url = f'https://www.airlinequality.com/airline-reviews/ana-all-nippon-airways/page/{page_number}/'
     response = requests.get(url)
@@ -249,9 +205,10 @@ def scrape_page_ANA(page_number):
 
     return reviews
 
-# %%
+# Main loop for ANA All Nippon Airways to scrape multiple pages and track progress
 ANA_reviews = []
 total_pages = 62
+print("Scrapping for ANA")
 for page_number in range(1, total_pages + 1):
     page_reviews = scrape_page_ANA(page_number)
     ANA_reviews.extend(page_reviews)
@@ -260,20 +217,9 @@ for page_number in range(1, total_pages + 1):
     if page_number % 10 == 0:
         print(f"{page_number} pages extraction completed")
 
-
-# %%
 ANA_reviews_df = pd.DataFrame(ANA_reviews)
 
-# %%
-ANA_reviews_df.sample(5)
-
-# %%
-ANA_reviews_df.shape
-
-# %% [markdown]
-# #6. Turkish Airlines
-
-# %%
+#Function for web scraping (Turkish Airlines)
 def scrape_page_Turk(page_number):
     url = f'https://www.airlinequality.com/airline-reviews/turkish-airlines/page/{page_number}/'
     response = requests.get(url)
@@ -302,9 +248,10 @@ def scrape_page_Turk(page_number):
 
     return reviews
 
-# %%
+# Main loop for turkish airlines to scrape multiple pages and track progress
 turkish_airlines_reviews = []
 total_pages = 280
+print("Scrapping for Turkish Airlines")
 for page_number in range(1, total_pages + 1):
     page_reviews = scrape_page_Turk(page_number)
     turkish_airlines_reviews.extend(page_reviews)
@@ -313,20 +260,9 @@ for page_number in range(1, total_pages + 1):
     if page_number % 10 == 0:
         print(f"{page_number} pages extraction completed")
 
-
-# %%
 turkish_airlines_reviews_df = pd.DataFrame(turkish_airlines_reviews)
 
-# %%
-turkish_airlines_reviews_df.sample(5)
-
-# %%
-turkish_airlines_reviews_df.shape
-
-# %% [markdown]
-# #7. Korean Air
-
-# %%
+#Function for web scraping (Korean Air)
 def scrape_page_KA(page_number):
     url = f'https://www.airlinequality.com/airline-reviews/korean-air/page/{page_number}/'
     response = requests.get(url)
@@ -355,9 +291,10 @@ def scrape_page_KA(page_number):
 
     return reviews
 
-# %%
+# Main loop for korean air to scrape multiple pages and track progress
 korean_air_reviews = []
 total_pages = 60
+print("Scrapping for Korean Air")
 for page_number in range(1, total_pages + 1):
     page_reviews = scrape_page_KA(page_number)
     korean_air_reviews.extend(page_reviews)
@@ -366,14 +303,9 @@ for page_number in range(1, total_pages + 1):
     if page_number % 10 == 0:
         print(f"{page_number} pages extraction completed")
 
-
-# %%
 korean_air_reviews_df = pd.DataFrame(korean_air_reviews)
 
-# %% [markdown]
-# #8. Air France
-
-# %%
+#Function for web scraping (Air France)
 def scrape_page_AF(page_number):
     url = f'https://www.airlinequality.com/airline-reviews/air-france/page/{page_number}/'
     response = requests.get(url)
@@ -402,9 +334,10 @@ def scrape_page_AF(page_number):
 
     return reviews
 
-# %%
+# Main loop for air france to scrape multiple pages and track progress
 air_france_reviews = []
 total_pages = 146
+print("Scrapping for Air France")
 for page_number in range(1, total_pages + 1):
     page_reviews = scrape_page_AF(page_number)
     air_france_reviews.extend(page_reviews)
@@ -413,14 +346,9 @@ for page_number in range(1, total_pages + 1):
     if page_number % 10 == 0:
         print(f"{page_number} pages extraction completed")
 
-
-# %%
 air_france_reviews_df = pd.DataFrame(air_france_reviews)
 
-# %% [markdown]
-# #9. Japan Airlines
-
-# %%
+#Function for web scraping (japan Airlines)
 def scrape_page_JA(page_number):
     url = f'https://www.airlinequality.com/airline-reviews/japan-airlines/page/{page_number}/'
     response = requests.get(url)
@@ -449,9 +377,10 @@ def scrape_page_JA(page_number):
 
     return reviews
 
-# %%
+# Main loop for japan airlines to scrape multiple pages and track progress
 japan_airlines_reviews = []
 total_pages = 44
+print("Scrapping for Japan Airlines")
 for page_number in range(1, total_pages + 1):
     page_reviews = scrape_page_JA(page_number)
     japan_airlines_reviews.extend(page_reviews)
@@ -460,14 +389,9 @@ for page_number in range(1, total_pages + 1):
     if page_number % 10 == 0:
         print(f"{page_number} pages extraction completed")
 
-
-# %%
 japan_airlines_reviews_df = pd.DataFrame(japan_airlines_reviews)
 
-# %% [markdown]
-# #10. Hainan Airlines
-
-# %%
+#Function for web scraping (Hainan Airlines)
 def scrape_page_HA(page_number):
     url = f'https://www.airlinequality.com/airline-reviews/hainan-airlines/page/{page_number}/'
     response = requests.get(url)
@@ -496,9 +420,10 @@ def scrape_page_HA(page_number):
 
     return reviews
 
-# %%
+# Main loop for hainan airlines to scrape multiple pages and track progress
 hainan_airlines_reviews = []
 total_pages = 43
+print("Scrapping for Hainan Airlines")
 for page_number in range(1, total_pages + 1):
     page_reviews = scrape_page_HA(page_number)
     hainan_airlines_reviews.extend(page_reviews)
@@ -508,13 +433,9 @@ for page_number in range(1, total_pages + 1):
         print(f"{page_number} pages extraction completed")
 
 
-# %%
 hainan_airlines_reviews_df = pd.DataFrame(hainan_airlines_reviews)
 
-# %% [markdown]
-# Combining Datasets together
-
-# %%
+# Combine all individual airlines dataframes
 qatar_airways_reviews_df.insert(0, 'Airlines', 'qatar_airways')
 singapore_airlines_reviews_df.insert(0, 'Airlines', 'singapore_airlines')
 cathay_pacific_airways_reviews_df.insert(0, 'Airlines', 'cathay_pacific_airlines')
@@ -526,7 +447,6 @@ air_france_reviews_df.insert(0, 'Airlines', 'air_france')
 japan_airlines_reviews_df.insert(0, 'Airlines', 'japan_airlines')
 hainan_airlines_reviews_df.insert(0, 'Airlines', 'hainan_airlines')
 
-# %%
 airlines_review = pd.concat([
     qatar_airways_reviews_df,
     singapore_airlines_reviews_df,
@@ -540,14 +460,5 @@ airlines_review = pd.concat([
     hainan_airlines_reviews_df
 ], axis=0, ignore_index=True)
 
-# %%
-airlines_review.sample(10)
-
-# %%
-airlines_review.shape
-
-# %%
-airlines_review.to_csv('airlines_review.csv', index=False)
-
-
-
+# Export out data to csv
+airlines_review.to_csv('data/airlines_review.csv', index=False)
